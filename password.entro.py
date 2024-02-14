86757aacc79ef1a2be7885241bb02ee1f4cd58ba
710052 <146425433+710052@users.noreply.github.com>
2024-02-14 08:52:23 +0000

    BIP39 Passphrase Generation Function

--- password.entro.py
+++ password.entro.py
@@ -9,4 +9,8 @@ def generate_random_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(characters) for _ in range(length))
 
-
+# BIP39 Passphrase Generation Function
+def generate_bip39_passphrase(word_list, num_words=12):
+    """Generate a random passphrase using the BIP39 wordlist."""
+    return ' '.join(random.choice(word_list) for _ in range(num_words))
+    
